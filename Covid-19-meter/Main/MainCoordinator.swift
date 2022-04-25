bac0438d8a8bcf96d5014e1b06a5e75813ccb65c
Radwa Ahmed <radwaahmed680@gmail.com>
2022-04-25 11:37:41 +0200

    Initial setup

---
+++ Covid-19-meter/Main/MainCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  MainCoordinator.swift
+//  Covid-19-meter
+//
+//  Created by Radwa Ahmed on 25/04/2022.
+//
+
+import Foundation
---
+++ Covid-19-meter/Main/MainFactory.swift
@@ -0,0 +1,8 @@
+//
+//  MainFactory.swift
+//  Covid-19-meter
+//
+//  Created by Radwa Ahmed on 25/04/2022.
+//
+
+import Foundation
